cac91a00307d10c2aadc0b1fd06615c70e2cd1fd
Christine Piilmann <christinepiilmann@Christines-MacBook-Air.local>
2020-12-29 23:02:41 +0100

    om side done og burger menu begyndt

---
+++ script.js
@@ -0,0 +1,8 @@
+window.addEventListener("load", sidenVises);
+
+
+function myFunction(toggleMenu) {
+    toggleMenu.classList.toggle("change");
+    document.querySelector("#navbar").classList.toggle("hidden");
+
+}
