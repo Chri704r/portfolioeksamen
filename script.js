18b1ffda3d440a0736002ee603c2005f6c2171b7
Christine Piilmann <christinepiilmann@Christines-MacBook-Air.local>
2021-01-05 10:50:05 +0100

    web portfolio done

--- script.js
+++ script.js
@@ -1,21 +1,46 @@
 window.addEventListener("load", sidenVises);
 
 // vi laver en variabel som vi vil bruge som kontakt (tænd og sluk) for burgermenuen
-let menuIsOpen;
+let menuIsOpen = false;
 
-// menuen starter med at være lukket, så vi sætter den til falsk
-menuIsOpen = false;
+//variabel til link
+let pilIsOut = false;
 
-
-let boolTest = true;
 
 
 function sidenVises() {
     console.log("sidenVises");
 
     document.querySelector("#menuknap").addEventListener("click", toggleMenu)
+    document.querySelector("#mere").addEventListener("mouseover", link);
+    document.querySelector("#mere").addEventListener("mouseout", linkIn);
 }
 
+function link() {
+    console.log("link");
+
+    document.querySelector("#mere").removeEventListener("mouseover", link)
+
+    document.querySelector("#pil").classList.remove("linkmovein");
+    document.querySelector("#pil").classList.add("linkmoveout");
+
+    document.querySelector("#mere").addEventListener("mouseout", linkIn);
+}
+
+function linkIn() {
+    console.log("linkIn");
+
+    document.querySelector("#mere").removeEventListener("mouseout", linkIn);
+
+    document.querySelector("#pil").classList.remove("linkmoveout");
+    document.querySelector("#pil").classList.add("linkmovein");
+
+    document.querySelector("#mere").addEventListener("mouseover", link);
+}
+
+
+
+// ------BURGER MENU---------
 
 function toggleMenu() {
     console.log("toggleMenu")
@@ -31,7 +56,7 @@ function toggleMenu() {
         document.querySelector("#navbar").classList.remove("openmenu");
         document.querySelector("#navbar").classList.add("closemenu");
 
-        // menuen er nu lukket, så ændr menuvariablen til at vise dette
+        // menuen er nu lukket, så ændrer menuvariablen til at vise dette
         menuIsOpen = false;
     } else {
 
@@ -42,7 +67,4 @@ function toggleMenu() {
         // Menuen er nu åben, så vi ændrer menuvariablen til at visse dette
         menuIsOpen = true;
     }
-
-    //    slideIsOut = !slideIsOut;
-
 }
